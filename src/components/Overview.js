7967fa7978f02818648ee25648fe51333d8b1426
Lucas <lucasmatiasblanco@outlook.com>
2022-11-14 13:02:38 -0300

    finished style to overview.js

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,38 +1,68 @@
 import React from "react"; 
+import { FaEnvelope, FaPhone } from "react-icons/fa";
+//import "../assets/Overview.scss"; 
 
 
 class Overview extends React.Component{
- 
+    
 
 
     render(){
-        const {fullname, email, phone, education, experience} = this.props.cv
+        
+        const {fullname, email, phone, education, experience} = this.props.cv;
+
         return( 
-            <div>
-                <div>{fullname}</div>
-                <div>{email}</div>
-                <div>{phone}</div>
-                {
-                    education.map((institution) => {
-                        return  <div key={institution.id}>
-                                <div>{institution.name}</div>
-                                <div>{institution.title}</div>
-                                <div>{institution.from}</div>
-                                <div>{institution.to}</div>
-                                </div>
-                    })
-                }
-                {
+            <div id='curriculum' className="curriculum">
+                <div className="name-heading">
+                    <h1 className="name">{fullname.toUpperCase()}</h1>
+                </div>
+                    <div className="container-information-contact">
+                        <div className ="email container-information">
+                            <div className="icon-information"><FaEnvelope /></div><div className="information-contact">{email}</div>
+                        </div>
+                        <div className="phone container-information">
+                            <div className="icon-information"><FaPhone /></div><div className="information-contact">{phone}</div>
+                        </div>
+                       
+                    </div>
+                    <div className="line-container"><hr></hr></div>
+                    <div className="field-container">
+                    <div className="field-name">
+                        <h2>WORK EXPERIENCE</h2>
+                    </div>
+                    {
                     experience.map((institution) => {
-                        return  <div key={institution.id}>
-                                <div>{institution.name}</div>
-                                <div>{institution.position}</div>
-                                <div>{institution.task}</div>
-                                <div>{institution.from}</div>
-                                <div>{institution.to}</div>
+                        return  <div key={institution.id} className="experience-field">
+                                    <div className="company-name">{institution.name}</div>
+                                    <div className="company-position">{institution.position}</div>
+                                    <div className="company-date-from">{institution.from}</div>
+                                    <div className="separation">-</div>
+                                    <div className="company-date-to">{institution.to}</div>
+                                    <div className="company-task">{institution.task}</div>
                                 </div>
                     })
                 }
+                    </div>
+                    <div className="field-container">
+                    <div className="field-name">
+                        <h2>EDUCATION</h2>
+                    </div>
+                    {
+                   
+                   education.map((institution) => {
+                       return  <div key={institution.id} className="education-field">
+                               <div className="institution-name">{institution.name}</div>
+                               <div className="institution-title">{institution.title}</div>
+                               <div className="institution-from">{institution.from}</div>
+                               <div className="separation">-</div>
+                               <div className="institution-to">{institution.to}</div>
+                               </div>
+                   })
+               }
+                    </div>
+                    
+                
+                
             </div> 
         )
     }
